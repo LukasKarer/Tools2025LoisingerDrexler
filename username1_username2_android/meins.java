5d0885e98d9d255f6d24aad6dec9392df67f59e0
Rainer <loisinger.r+Laptop@gmail.com>
2025-05-22 09:05:06 +0200

    added console output to java method

--- username1_username2_android/meins.java
+++ username1_username2_android/meins.java
@@ -1,4 +1,5 @@
 // public static void main string args public static void main string args public static void main string args
 public int getAge(String name) {
+    System.out.println("getAge was called");
     return name.length();
 }
